8692bf87910fd2cf138839aeec0e3eeb38aa6e15
Jocimar B. A. Huss <jocimarspi@gmail.com>
2021-01-24 18:37:02 -0300

    09 RestWithSpringbootUdemy Content negotiation

---
+++ 09-rest-with-springboot-content_negotiation/src/main/java/br/com/jocimar/config/WebConfig.java
@@ -0,0 +1,54 @@
+package br.com.jocimar.config;
+
+import java.util.List;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import br.com.jocimar.converter.YamlJackson2HttpMessageConverter;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer{
+	
+	private static final MediaType MEDIA_TYPE_YML = MediaType.valueOf("application/x-yaml");
+	
+	public void extendedMessageConverter(List<HttpMessageConverter<?>> converters) {
+		converters.add(new YamlJackson2HttpMessageConverter());		
+	}
+	
+	@Override
+	public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+		
+		// USING EXTENSION		
+		/*
+		 * configurer.favorParameter(false) .ignoreAcceptHeader(false)
+		 * .defaultContentType(MediaType.APPLICATION_JSON) .mediaType("json",
+		 * MediaType.APPLICATION_JSON) .mediaType("xml", MediaType.APPLICATION_XML)
+		 * .mediaType("x-yaml", MEDIA_TYPE_YML);
+		 */	 
+		
+		// USING URL PARAMETER
+		/*
+		 * configurer.favorPathExtension(false) .favorParameter(true)
+		 * .parameterName("mediaType") .ignoreAcceptHeader(true)
+		 * .defaultContentType(MediaType.APPLICATION_JSON) .mediaType("json",
+		 * MediaType.APPLICATION_JSON) .mediaType("xml", MediaType.APPLICATION_XML)
+		 * .mediaType("x-yaml", MEDIA_TYPE_YML);
+		 */
+		
+		// USING HEADER PARAMETER
+		configurer.favorPathExtension(false)
+		  .favorParameter(false)
+		  .ignoreAcceptHeader(false) 
+		  .useRegisteredExtensionsOnly(false)
+		  .defaultContentType(MediaType.APPLICATION_JSON)
+		  .mediaType("json", MediaType.APPLICATION_JSON)
+		  .mediaType("xml", MediaType.APPLICATION_XML)
+		  .mediaType("x-yaml", MEDIA_TYPE_YML);
+	}	
+}
---
+++ 09-rest-with-springboot-content_negotiation/src/main/java/br/com/jocimar/services/PersonService.java
@@ -0,0 +1,60 @@
+package br.com.jocimar.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.jocimar.adapter.DozerAdapter;
+import br.com.jocimar.data.model.Person;
+import br.com.jocimar.data.vo.v1.PersonVO;
+import br.com.jocimar.exceptions.ResourceNotFoundException;
+import br.com.jocimar.repository.PersonRepository;
+
+@Service
+public class PersonService {
+	
+	@Autowired
+	PersonRepository repository;
+	
+	public PersonVO create(PersonVO person) {
+		var entity = DozerAdapter.parseObject(person, Person.class);		
+		Person savedPerson = repository.save(entity);		
+		var vo = DozerAdapter.parseObject(savedPerson, PersonVO.class);		
+		
+		return vo;
+	}
+	
+	public PersonVO update(PersonVO person) {
+		var entity = repository.findById(person.getId())
+				.orElseThrow(() -> new ResourceNotFoundException("No record found for this ID."));
+		
+		entity.setFirstName(person.getFirstName());
+		entity.setLastName(person.getLastName());
+		entity.setAddress(person.getAddress());
+		entity.setGender(person.getGender());
+		
+		var savedPerson = repository.save(entity);
+		
+		return DozerAdapter.parseObject(savedPerson, PersonVO.class);
+	}
+	
+	public void delete(Long id) {
+		Person entity = repository.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("No record found for this ID."));
+		
+		repository.delete(entity);
+	}
+	
+	public List<PersonVO> findAll() {		
+		var personList = repository.findAll();		
+		return DozerAdapter.parseListObjects(personList, PersonVO.class);
+	}
+	
+	public PersonVO findById(Long id) {
+		var entity = repository.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("No record found for this ID."));
+		
+		return DozerAdapter.parseObject(entity, PersonVO.class); 
+	}
+}
